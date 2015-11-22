ac804824f7c38b608fd32ce848c0173f944187a6
Andy VanWagoner <thetalecrafter@gmail.com>
2015-11-21 21:53:20 -0700

    fix #28, stop mutating the settings

--- packages/eslint-plugin-format-message/lib/util/get-settings.js
+++ packages/eslint-plugin-format-message/lib/util/get-settings.js
@@ -5,35 +5,42 @@ var generate = require('format-message-generate-id')
 
 module.exports = function getSettings (context) {
   var settings = context.settings['format-message'] || {}
-  if (typeof settings.generateId !== 'function') {
-    var translations = settings.translations || (settings.translations = {})
-    if (typeof translations === 'string') {
-      try {
-        translations = settings.translations =
-          require(pathUtil.resolve(translations))
-      } catch (err) {
-        console.warn(err.message)
-        translations = settings.translations = {}
-      }
+  var resolved = {}
+
+  resolved.sourceLocale = settings.sourceLocale
+
+  var translations = {}
+  if (typeof settings.translations === 'string') {
+    try {
+      translations = require(pathUtil.resolve(settings.translations))
+    } catch (err) {
+      console.warn(err.message)
     }
-    Object.keys(translations).forEach(function (locale) {
-      if (typeof translations[locale] === 'string') {
+  } else if (typeof settings.translations === 'object') {
+    Object.keys(settings.translations).forEach(function (locale) {
+      var localeData = settings.translations[locale]
+      if (typeof localeData === 'string') {
         try {
-          translations[locale] = require(pathUtil.resolve(translations[locale]))[locale]
+          localeData = require(pathUtil.resolve(localeData))
+          if (localeData[locale]) localeData = localeData[locale]
         } catch (err) {
+          localeData = {}
           console.warn(err.message)
-          translations[locale] = null
         }
       }
+      translations[locale] = localeData
     })
-    var generateId = generate[settings.generateId] || generate.underscored_crc32
-    settings.generateId = function (pattern) {
-      try {
-        return generateId(pattern)
-      } catch (_) {
-        return null
-      }
+  }
+  resolved.translations = translations
+
+  var generateId = generate[settings.generateId] || generate.underscored_crc32
+  resolved.generateId = function (pattern) {
+    try {
+      return generateId(pattern)
+    } catch (_) {
+      return null
     }
   }
-  return settings
+
+  return resolved
 }
