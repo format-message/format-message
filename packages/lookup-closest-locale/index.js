865d1593365518038ee663a71bf183a469c5eb87
Andy VanWagoner <andy@thetalecrafter.com>
2018-04-11 14:59:33 -0600

    style: clean up lookup and add flow types

--- packages/lookup-closest-locale/index.js
+++ packages/lookup-closest-locale/index.js
@@ -1,13 +1,14 @@
+// @flow
 // "lookup" algorithm http://tools.ietf.org/html/rfc4647#section-3.4
-module.exports = function lookupClosestLocale (locale, available) {
-  if (available[locale]) return locale
-  var locales = [].concat(locale || [])
-  for (var l = 0, ll = locales.length; l < ll; ++l) {
-    var current = locales[l].split('-')
+// assumes normalized language tags, and matches in a case sensitive manner
+module.exports = function lookupClosestLocale (locale/*: string | string[] | void */, available/*: { [string]: any } */)/*: string */ {
+  if (typeof locale === 'string' && available[locale]) return locale
+  const locales = [].concat(locale || [])
+  for (let l = 0, ll = locales.length; l < ll; ++l) {
+    const current = locales[l].split('-')
     while (current.length) {
-      if (current.join('-') in available) {
-        return current.join('-')
-      }
+      const candidate = current.join('-')
+      if (available[candidate]) return candidate
       current.pop()
     }
   }
