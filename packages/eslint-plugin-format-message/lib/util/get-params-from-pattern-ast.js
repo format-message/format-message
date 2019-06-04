724af378dcedce640dc184c0427796083ae316d7
Brandon Johnson <bjohn465+github@gmail.com>
2019-06-04 09:31:08 -0600

    Handle rich text params in match params rule (#262)
    fixes #256

--- packages/eslint-plugin-format-message/lib/util/get-params-from-pattern-ast.js
+++ packages/eslint-plugin-format-message/lib/util/get-params-from-pattern-ast.js
@@ -1,6 +1,22 @@
 'use strict'
 
-module.exports = function getParamsFromPatternAst (ast) {
+var richTagCache = []
+function getRichTag (name) {
+  var cachedTag = richTagCache.find(function (cacheEntry) {
+    return cacheEntry.name === name
+  })
+  if (cachedTag) return cachedTag
+
+  var tag = {
+    isRich: true,
+    name: name,
+    toString: function () { return '<' + name + '>' }
+  }
+  richTagCache.push(tag)
+  return tag
+}
+
+module.exports = function getParamsFromPatternAst (ast, isRich) {
   if (!ast || !ast.slice) return []
   var stack = ast.slice()
   var params = []
@@ -10,11 +26,17 @@ module.exports = function getParamsFromPatternAst (ast) {
     if (element.length === 1 && element[0] === '#') continue
 
     var name = element[0]
+    var type = element[1]
+    var isRichParam = isRich && type === '<>'
+
+    if (isRichParam) {
+      name = getRichTag(name)
+    }
     if (params.indexOf(name) < 0) params.push(name)
 
-    var type = element[1]
-    if (type === 'select' || type === 'plural' || type === 'selectordinal') {
-      var children = type === 'select' ? element[2] : element[3]
+    if (type === 'select' || type === 'plural' || type === 'selectordinal' || isRichParam) {
+      var children = type === 'select' || isRichParam ? element[2] : element[3]
+      if (!children) continue
       stack = stack.concat.apply(stack,
         Object.keys(children).map(function (key) { return children[key] })
       )
