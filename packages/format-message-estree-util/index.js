76b551e9b09e276bcfcf99e7a832cf982eb13340
Ahmad Amireh <ahmad@amireh.net>
2021-06-23 16:48:44 -0600

    ignore "format-message" file extension during parse
    the parser is not following imports that specify "format-message.js",
    which is the output of recent babel transpilation

--- packages/format-message-estree-util/index.js
+++ packages/format-message-estree-util/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var MODULE_NAME_PATTERN = /(^|\/)format-message$/
+var MODULE_NAME_PATTERN = /(^|\/)format-message(\.js)?$/
 
 exports = module.exports = {
   setBabelContext: function (path, state) {
